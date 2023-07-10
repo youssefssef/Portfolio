e6dddbff6789136433ec434c95308d1a993b64cf
youssefSsef <youssef.online10@gmail.com>
2023-07-10 11:13:53 +0100

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
