338765508da0a5e90ae50a9df692b20648c6864e
youssefSsef <youssef.online10@gmail.com>
2023-07-11 12:07:27 +0100

    main

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/controllers/flip_card_controllers.dart';
+import 'package:flutter_flip_card/flipcard/flip_card.dart';
+import 'package:flutter_flip_card/modal/flip_side.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
@@ -13,15 +16,38 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<String> menu = ['Project', 'Resume', 'Contact', 'About'];
-  List<Color> colorizeColors = [
-    Colors.purple,
-    Colors.blue,
-    Colors.yellow,
-    Colors.red,
+  List<String> skils = ["UI/UX", "FireBase", "API", "Github", "Git", "SQLite"];
+  List<Image> skilsImage = [
+    Image.asset('assets/ui.png'),
+    Image.asset('assets/firebase.png'),
+    Image.asset('assets/api.png'),
+    Image.asset('assets/github.png'),
+    Image.asset('assets/git.jpg'),
+    Image.asset('assets/sQLite.jpg')
   ];
+  late List<bool> _isHovered;
+  final controller = FlipCardController();
+
+  @override
+  void initState() {
+    super.initState();
+    _isHovered = List.filled(6, false);
+  }
+
+  void _toggleHover(int index, bool isHovered) {
+    setState(() {
+      _isHovered[index] = isHovered;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Color defaultColor = Colors.white;
+    Color containerhover = Color.fromARGB(255, 61, 4, 196);
+    Color shadowColor = Colors.black.withOpacity(0.3);
+    double shadowOffset = 5.0;
+    double shadowBlurRadius = 5.0;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 31, 1, 101),
@@ -34,7 +60,7 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: List.generate(menu.length, (index) {
               return SizedBox(
-                width: 200,
+                width: MediaQuery.of(context).size.width / 8,
                 child: TextButton(
                     onPressed: () {},
                     style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith<Color>(
@@ -126,6 +152,7 @@ class _HomePageState extends State<HomePage> {
             Container(
               height: 500,
               width: MediaQuery.of(context).size.width,
+              color: Color.fromARGB(255, 238, 235, 235),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -164,20 +191,65 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Expanded(
                     child: GridView.builder(
-                      itemCount: 10, // Number of items in the grid
+                      itemCount: skils.length, // Number of items in the grid
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4, // Number of columns
+                        crossAxisCount: MediaQuery.of(context).size.width > 600
+                            ? 4
+                            : MediaQuery.of(context).size.width > 400
+                                ? 3
+                                : 2, // Number of columns
                       ),
                       itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: EdgeInsets.all(25.0),
-                          color: Colors.blue,
-                          child: Center(
-                            child: Text(
-                              'Item $index',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
+                        return MouseRegion(
+                          onEnter: (_) => _toggleHover(index, true),
+                          onExit: (_) => _toggleHover(index, false),
+                          child: FlipCard(
+                            rotateSide: RotateSide.bottom,
+                            onTapFlipping: true, //When enabled, the card will flip automatically when touched.
+                            axis: FlipAxis.horizontal,
+                            controller: controller,
+                            frontWidget: Container(
+                              margin: EdgeInsets.all(25.0),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  boxShadow: _isHovered[index]
+                                      ? [
+                                          BoxShadow(
+                                            color: shadowColor,
+                                            offset: Offset(shadowOffset, shadowOffset),
+                                            blurRadius: shadowBlurRadius,
+                                          ),
+                                        ]
+                                      : null,
+                                  image: DecorationImage(image: skilsImage[index].image, fit: BoxFit.cover)),
+                            ),
+                            backWidget: Container(
+                              margin: EdgeInsets.all(25.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: _isHovered[index] ? containerhover : defaultColor,
+                                boxShadow: _isHovered[index]
+                                    ? [
+                                        BoxShadow(
+                                          color: shadowColor,
+                                          offset: Offset(shadowOffset, shadowOffset),
+                                          blurRadius: shadowBlurRadius,
+                                        ),
+                                      ]
+                                    : null,
+                              ),
+                              child: Center(
+                                child: FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(
+                                    skils[index],
+                                    style: TextStyle(
+                                      color: _isHovered[index] ? Colors.white : Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
